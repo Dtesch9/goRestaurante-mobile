4534a15a568e0d086b3074fd4fb921668aaca644
Dtesch9 <alucard.s8123@gmail.com>
2020-06-20 13:15:39 -0300

    chore: Load Orders

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -23,7 +23,7 @@ interface Food {
   name: string;
   description: string;
   price: number;
-  formattedValue: number;
+  formattedPrice: string;
   thumbnail_url: string;
 }
 
@@ -33,6 +33,14 @@ const Orders: React.FC = () => {
   useEffect(() => {
     async function loadOrders(): Promise<void> {
       // Load orders from API
+      const response = await api.get<Omit<Food, 'formattedValue'>[]>('orders');
+
+      const data = response.data.map(order => ({
+        ...order,
+        formattedPrice: formatValue(order.price),
+      }));
+
+      setOrders(data);
     }
 
     loadOrders();
